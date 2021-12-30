de8eae286cb8ec7f98f3cd82f63fe453ff5dd6f3
Rodrigo Machado <rodrigo.cmachado@gmail.com>
2021-12-29 23:23:54 -0300

    User entity and resource

---
+++ src/main/java/com/educandoweb/cursojava/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.educandoweb.cursojava.resources;
+
+import com.educandoweb.cursojava.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+public ResponseEntity<User> findAll(){
+    User user = new User(1L, "Rodrigo", "rodrigo@gmail.com", "9999999", "12345");
+    return ResponseEntity.ok().body(user);
+}
+
+
+
+
+}
