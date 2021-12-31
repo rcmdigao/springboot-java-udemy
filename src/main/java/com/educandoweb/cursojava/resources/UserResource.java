74cbfc057718f92ee3ef6f7e5b3177ee6926261a
Rodrigo Machado <rodrigo.cmachado@gmail.com>
2021-12-31 19:39:57 -0300

    User delete

--- src/main/java/com/educandoweb/cursojava/resources/UserResource.java
+++ src/main/java/com/educandoweb/cursojava/resources/UserResource.java
@@ -39,5 +39,17 @@ public class UserResource {
         return ResponseEntity.created(uri).body(obj);
     }
 
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        // resposta vazia
+        return ResponseEntity.noContent().build();
+    }
+
+
+
+
+
+
 
 }
--- src/main/java/com/educandoweb/cursojava/services/UserService.java
+++ src/main/java/com/educandoweb/cursojava/services/UserService.java
@@ -27,5 +27,9 @@ public class UserService {
         return userRepository.save(obj);
     }
 
+    public void delete(Long id){
+        userRepository.deleteById(id);
+    }
+
 
 }
