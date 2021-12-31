79d8e750bd89d9545efc1f6c7c4b67592539ab14
Rodrigo Machado <rodrigo.cmachado@gmail.com>
2021-12-31 19:48:16 -0300

    User update

--- src/main/java/com/educandoweb/cursojava/services/UserService.java
+++ src/main/java/com/educandoweb/cursojava/services/UserService.java
@@ -31,5 +31,17 @@ public class UserService {
         userRepository.deleteById(id);
     }
 
+    public User update(Long id, User obj){
+        User entity = userRepository.getOne(id); // getOne: Prepara o objeto monitorado e depois ele efetua a operacao no BD
+        updateData(entity, obj);
+        return userRepository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
+    }
+
 
 }
